d16837e797fcf25f87f62e3b42da01bb51b404d7
seleonel <leon_bellini@outlook.com>
2018-08-28 16:09:02 -0300

    adicionado javascript para foco e botões para tal, conflitos foram removidos

--- scripts/joojscript.js
+++ scripts/joojscript.js
@@ -7,3 +7,10 @@ function leiamenos(variavel_segunda, botaodesativado){
   document.getElementById(variavel_segunda).style.display = 'none';
   document.getElementById(botaodesativado).style.display = 'block';
 }
+
+function focopag(elemento){
+  var parte_focar = document.getElementById(elemento);
+  parte_focar.scrollIntoView();
+
+
+}
